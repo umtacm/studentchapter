fb21432eec336b7a4c05fd38ae88e8d78218d054
umtacm <umtacmstudentchapter@gmail.com>
2021-12-08 23:25:49 +0500

    Updates

---
+++ precache-manifest.7c0abfb17408494d09af4befd250d51d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5478d8b3cf00b8be32fe6ece662bc7e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07a6f5425551bf5d0122",
+    "url": "/static/css/main.b85d456e.chunk.css"
+  },
+  {
+    "revision": "58c517cd1730881ae8c6",
+    "url": "/static/js/2.63c8b0ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.63c8b0ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a6f5425551bf5d0122",
+    "url": "/static/js/main.798e3b08.chunk.js"
+  },
+  {
+    "revision": "8e055f39f1ae47ded08f",
+    "url": "/static/js/runtime-main.48902787.js"
+  },
+  {
+    "revision": "f395300fd493faf1bd0e05c896e79cef",
+    "url": "/static/media/acm-logo.f395300f.png"
+  },
+  {
+    "revision": "ae2508775e7b40b7f9678a27610e3578",
+    "url": "/static/media/bgVideo.ae250877.mp4"
+  },
+  {
+    "revision": "d357be2d7323baf780b21b1cf4763b04",
+    "url": "/static/media/classFour.d357be2d.jpg"
+  },
+  {
+    "revision": "c53087e17b31991a77745ecb0bc41c42",
+    "url": "/static/media/classOne.c53087e1.JPG"
+  },
+  {
+    "revision": "04fed69d728840c753440ecdd7e0c6f9",
+    "url": "/static/media/classThree.04fed69d.jpg"
+  },
+  {
+    "revision": "1c334cb659967f406091e9e454d19a64",
+    "url": "/static/media/classTwo.1c334cb6.jpg"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/static/media/contactBg.31cb05f3.jpg"
+  },
+  {
+    "revision": "98c43ec60db17a27c9e7acd280f83e3e",
+    "url": "/static/media/dumbbell.98c43ec6.png"
+  },
+  {
+    "revision": "caad112825eea01fa4ddac1f21225efc",
+    "url": "/static/media/gymOverlayBg.caad1128.jpg"
+  },
+  {
+    "revision": "98c43ec60db17a27c9e7acd280f83e3e",
+    "url": "/static/media/logoumtacm.98c43ec6.png"
+  },
+  {
+    "revision": "ef1bd7be8dcf17849f9a0f598b9db278",
+    "url": "/static/media/patron.ef1bd7be.jpg"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/static/media/scheduleBg.02e36482.jpg"
+  },
+  {
+    "revision": "c53087e17b31991a77745ecb0bc41c42",
+    "url": "/static/media/slide_1.c53087e1.JPG"
+  },
+  {
+    "revision": "febee0ecf8d1f99f0a0dff24e748b236",
+    "url": "/static/media/slide_2.febee0ec.JPG"
+  },
+  {
+    "revision": "f97c1d20489d912d343b3276824f6589",
+    "url": "/static/media/slide_3.f97c1d20.jpg"
+  },
+  {
+    "revision": "9400750f90e4d86a02af2aa9be882a24",
+    "url": "/static/media/slide_4.9400750f.JPG"
+  },
+  {
+    "revision": "04fed69d728840c753440ecdd7e0c6f9",
+    "url": "/static/media/slide_5.04fed69d.jpg"
+  },
+  {
+    "revision": "7b6e0461f379b00d48d214b6d7f214e9",
+    "url": "/static/media/slide_6.7b6e0461.jpg"
+  },
+  {
+    "revision": "0f04e8cfa3a27f35210358ea0663e4ac",
+    "url": "/static/media/slide_7.0f04e8cf.jpg"
+  },
+  {
+    "revision": "98c43ec60db17a27c9e7acd280f83e3e",
+    "url": "/static/media/trainerOne.98c43ec6.jpg"
+  },
+  {
+    "revision": "98c43ec60db17a27c9e7acd280f83e3e",
+    "url": "/static/media/trainerThree.98c43ec6.jpg"
+  },
+  {
+    "revision": "98c43ec60db17a27c9e7acd280f83e3e",
+    "url": "/static/media/trainerTwo.98c43ec6.jpg"
+  }
+]);
